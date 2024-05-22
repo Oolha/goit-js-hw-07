d3b95b46b042d6bb11d94ddecf37cf16a3c5f3a7
olha_sydorchuk <157120293+Oolha@users.noreply.github.com>
2024-05-23 00:19:43 +0200

    did all tasks

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,22 @@
+
+
+const changeColorBtn = document.querySelector(".change-color");
+const newValue = document.querySelector(".color");
+
+changeColorBtn.addEventListener('click', function () {
+  document.body.style.backgroundColor = getRandomHexColor();
+  newValue.textContent = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+  
+  
+  
 }
+
+
+
+
--- js/task-6.js
+++ js/task-6.js
@@ -1,5 +1,39 @@
+const createValue = document.querySelector(".controls-btn");
+const destroyValueBtn = document.querySelector(".destroy-btn");
+const inputValue = document.querySelector("input");
+const divValue = document.querySelector("#boxes");
+
+createValue.addEventListener('click', function () {
+  const amount = Number(inputValue.value);
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    inputValue.value = '';
+  }
+})
+
+
+function createBoxes(amount) {
+  divValue.innerHTML = '';
+  for (let i = 0; i < amount; i++) {
+    const boxes = document.createElement('div');
+    boxes.style.width = `${30 + i * 10}px`;
+    boxes.style.height = `${30 + i * 10}px`;
+    boxes.style.backgroundColor = getRandomHexColor();
+    divValue.appendChild(boxes);
+
+  }
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+destroyValueBtn.addEventListener('click', destroyElem);
+
+function destroyElem() {
+  divValue.innerHTML = '';
+}
+
+
+
